b21c2a306fc71a2038835f3c90a68e33db42d241
jslightham <31053827+jslightham@users.noreply.github.com>
2019-03-29 11:29:03 -0400

    Update Fractal Tree.py

--- Fractal Tree.py
+++ Fractal Tree.py
@@ -1,18 +1,19 @@
 import turtle             
-wn = turtle.Screen()        
+wn = turtle.Screen()
 t = turtle.Turtle()
+t.speed('fastest')
 t.left(90)
-t.back(50)
+t.back(150)
 def draw(l):
     if(l < 1):
         return;
     else:
         t.forward(l)
         t.right(30)
-        draw(l-15)
+        draw(l/1.5)
         t.left(60)
-        draw(l-15)
+        draw(l/1.5)
         t.right(30)
         t.backward(l)
 
-draw(100);
+draw(125);
